4526075439495d0d51986f4b7a7b8d923cc7a630
ImKunYoung <ur2kunyoung2@outlook.com>
2022-10-26 19:57:24 +0900

    init commit -rental

---
+++ rental/src/test/java/com/my/rental/cucumber/CucumberIT.java
@@ -0,0 +1,8 @@
+package com.my.rental.cucumber;
+
+import com.my.rental.IntegrationTest;
+import io.cucumber.junit.platform.engine.Cucumber;
+
+@Cucumber
+@IntegrationTest
+class CucumberIT {}
---
+++ rental/src/test/java/com/my/rental/cucumber/CucumberTestContextConfiguration.java
@@ -0,0 +1,10 @@
+package com.my.rental.cucumber;
+
+import com.my.rental.IntegrationTest;
+import io.cucumber.spring.CucumberContextConfiguration;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@CucumberContextConfiguration
+@IntegrationTest
+@WebAppConfiguration
+public class CucumberTestContextConfiguration {}
