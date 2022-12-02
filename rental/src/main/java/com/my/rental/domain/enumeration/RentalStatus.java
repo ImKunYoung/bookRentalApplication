cb6986a0f58cf0fe8e12df711dbcd33d8194909b
ImKunYoung <ur2kunyoung2@outlook.com>
2022-12-02 15:28:42 +0900

    표준 타입 정의

--- rental/src/main/java/com/my/rental/domain/enumeration/RentalStatus.java
+++ rental/src/main/java/com/my/rental/domain/enumeration/RentalStatus.java
@@ -4,6 +4,6 @@ package com.my.rental.domain.enumeration;
  * The RentalStatus enumeration.
  */
 public enum RentalStatus {
-    RENT_AVAILABLE,
-    RENT_UNAVAILABE,
+    RENT_AVAILABLE(0, "대출가능", "대출가능상태"),
+    RENT_UNAVAILABE(1, "대출불가", "대출불가상태");"),
 }
