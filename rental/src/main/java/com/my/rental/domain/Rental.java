8dd484eee7a630316adab7bcc85e62014f8dc328
ImKunYoung <ur2kunyoung2@outlook.com>
2023-01-08 12:29:19 +0900

    style: Change if Command Style

--- rental/src/main/java/com/my/rental/domain/Rental.java
+++ rental/src/main/java/com/my/rental/domain/Rental.java
@@ -162,9 +162,16 @@ public class Rental implements Serializable {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        if (this == o) {
+            return true;
+        }
+
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) {
+            return false;
+        }
+
         Rental rental = (Rental) o;
+
         return id != null && Objects.equals(id, rental.id);
     }
 
